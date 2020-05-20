9aa99325a254c66a48150475fa9d8c4a000ea0d7
jmacey <jmacey@bournemouth.ac.uk>
2020-05-20 12:29:15 +0100

    moved located code to lecture2 not 3

---
+++ Lecture2/TriLocatorNode/include/TriLocatorNodeData.h
@@ -0,0 +1,31 @@
+#ifndef TRILOCATORNODEDATA_H_
+#define TRILOCATORNODEDATE_H_
+
+#include <maya/MUserData.h>
+#include <maya/MColor.h>
+#include <maya/MPointArray.h>
+#include <maya/MVectorArray.h>
+#include <maya/MColorArray.h>
+#include <memory>
+class TriLocatorNodeData : public MUserData
+{
+public:
+	TriLocatorNodeData() : MUserData(false) 
+	{ 
+		m_normalArray = new MVectorArray(); 
+		m_colourArray = new MColorArray();
+	} // don't delete after draw
+	~TriLocatorNodeData() override 
+	{ 
+		delete m_normalArray; 
+		delete m_colourArray;
+	}
+
+	//MColor m_colour;
+	MPointArray m_lineArray;
+	MPointArray m_triangleArray;
+	MVectorArray *m_normalArray; // raw pointers can we use smart ones?
+	MColorArray *m_colourArray;
+};
+
+#endif

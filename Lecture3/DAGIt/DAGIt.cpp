2a94c1a3b84957e6636204382d9ebe212e763b79
jmacey <jmacey@bournemouth.ac.uk>
2024-02-27 20:35:19 +0000

    updated the DAGit demo

--- Lecture3/DAGIt/DAGIt.cpp
+++ Lecture3/DAGIt/DAGIt.cpp
@@ -1,6 +1,7 @@
 #include <maya/MSimple.h>
 #include <maya/MGlobal.h>
 #include <maya/MString.h>
+#include <maya/MStringArray.h>
 #include <maya/MDagPath.h>
 #include <maya/MFnDagNode.h>
 #include <maya/MSelectionList.h>
@@ -20,42 +21,75 @@ MStatus DAGit::doIt( const MArgList& )
   MFnDagNode nodeFn;
   MObject node;
   uint count=0;
-  char msg[50];
-
+  MString message;
+  // numeric arguments are harder to use, we can format using the following
+  // MStringArray and set the arguments we need in the message string
+  // using format. We have 4 arguments max with a matrix but we can use less if we want
+  MStringArray args(4,"");
   MGlobal::displayInfo("traversing DAG");
   for (dagIt.next(); !dagIt.isDone(); dagIt.next())
   {
-    node = dagIt.item();
+    // note item() is now deprecated so we use currentItem()
+    node = dagIt.currentItem();
     nodeFn.setObject( node );
     if( node.hasFn(MFn::kNamedObject))
     {
+      // for simple string objects formatting can be done with + 
       MGlobal::displayInfo("Object "+nodeFn.name()+ " type " + nodeFn.typeName());
       MGlobal::displayInfo("fullPathName "+nodeFn.fullPathName());
       MGlobal::displayInfo("partialPathName "+nodeFn.partialPathName());
       MGlobal::displayInfo("Parent path "+nodeFn.partialPathName());
       MGlobal::displayInfo("Parent namespace "+nodeFn.parentNamespace());
-      sprintf(msg,"number of children= %d number of parents= %d ",nodeFn.childCount(),nodeFn.parentCount());
-      MGlobal::displayInfo(msg);
-      sprintf(msg,"attribute count = %d ",nodeFn.attributeCount());
-      MGlobal::displayInfo(msg);
+      // numeric arguments need conversion so we use the args string array.
+      args[0].set(nodeFn.childCount());
+      args[1].set(nodeFn.parentCount());
+      // followed by the format with the ^1s string marker for the first argument 
+      // and the ^2s string marker for the second argument etc.
+      message.format("number of children= ^1s number of parents= ^2s ",args);     
+      MGlobal::displayInfo(message);
+
+      args[0].set(nodeFn.attributeCount());
+      message.format("attribute count = ^1s ",args);     
+      MGlobal::displayInfo(message);
       MGlobal::displayInfo("Transformation Matrix");
       MMatrix m=nodeFn.transformationMatrix();
-      sprintf(msg,"[%02.4lf,%02.4lf,%02.4lf,%02.4lf]",m(0,0),m(0,1),m(0,2),m(0,3));
-      MGlobal::displayInfo( msg);
-      sprintf(msg,"[%02.4lf,%02.4lf,%02.4lf,%02.4lf]",m(1,0),m(1,1),m(1,2),m(1,3));
-      MGlobal::displayInfo( msg);
-      sprintf(msg,"[%02.4lf,%02.4lf,%02.4lf,%02.4lf]",m(2,0),m(2,1),m(2,2),m(2,3));
-      MGlobal::displayInfo( msg);
-      sprintf(msg,"[%02.4lf,%02.4lf,%02.4lf,%02.4lf]",m(3,0),m(3,1),m(3,2),m(3,3));
-      MGlobal::displayInfo( msg);
+      // set row 1 args
+      args[0].set(m(0,0));
+      args[1].set(m(0,1));
+      args[2].set(m(0,2));
+      args[3].set(m(0,3));
+      message.format("[^1s,^2s,^3s,^4s]",args);
+      MGlobal::displayInfo(message);
+      // set row 2 args
+      args[0].set(m(1,0));
+      args[1].set(m(1,1));
+      args[2].set(m(1,2));
+      args[3].set(m(1,3));
+      message.format("[^1s,^2s,^3s,^4s]",args);
+      MGlobal::displayInfo(message);
+      // set row 3 args
+      args[0].set(m(2,0));
+      args[1].set(m(2,1));
+      args[2].set(m(2,2));
+      args[3].set(m(2,3));
+      message.format("[^1s,^2s,^3s,^4s]",args);
+      MGlobal::displayInfo(message);
+      // set row 4 args
+      args[0].set(m(3,0));
+      args[1].set(m(3,1));
+      args[2].set(m(3,2));
+      args[3].set(m(3,3));
+      message.format("[^1s,^2s,^3s,^4s]",args);
+      MGlobal::displayInfo(message);
 
       MGlobal::displayInfo("*********************************************************************");
       ++count;
     }
 
   }
-  sprintf(msg,"found %d objects",count);
-  MGlobal::displayInfo(msg);
+  args[0].set(count);
+  message.format("found ^1s objects",args);
+  MGlobal::displayInfo(message);
 
   return MS::kSuccess;
 }

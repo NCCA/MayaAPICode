fa89a6de78ab546c5b6ae926af6f9c34869f6691
jmacey <jmacey@bournemouth.ac.uk>
2024-02-27 21:31:55 +0000

    updated timeline demo to use snprintf may use fmt instead as example

--- Lecture3/Frames/TimeLine.cpp
+++ Lecture3/Frames/TimeLine.cpp
@@ -23,16 +23,16 @@ MStatus TimeLine::doIt( const MArgList& )
 
   // now lets accesss this info
   char msg[100];
-  sprintf(msg,"start frame =%f",anim.animationStartTime().value() );
+  snprintf(msg,100,"start frame =%f",anim.animationStartTime().value() );
   MGlobal::displayInfo(msg);
-  sprintf(msg,"end frame =%f",anim.animationEndTime().value() );
+  snprintf(msg,100,"end frame =%f",anim.animationEndTime().value() );
   MGlobal::displayInfo(msg);
   int end=anim.animationEndTime().value();
   for(float i=0.0f; i<end; i+=0.1)
   {
     currframe.setValue(i);
     anim.setCurrentTime(currframe);
-    sprintf(msg,"frame =%f",anim.currentTime().value() );
+    snprintf(msg,100,"frame =%f",anim.currentTime().value() );
     MGlobal::displayInfo(msg);
   }
 
